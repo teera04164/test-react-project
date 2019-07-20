ef0ad9cb629b7bc1d962eb2c6142026ecb15c211
teera <teera04164@gmail.com>
2019-07-20 12:44:00 +0700

    input dan 1

--- src/App.js
+++ src/App.js
@@ -12,10 +12,67 @@ class App extends Component {
 	}
 
 	printPreview = async () => {
+		
+		let a1 = "2[abc]3[ab]c"
+		let a2 = "2[3[a]b]"
+		let a3 = '10[a]c2[ab]'
+		let count
+		let print = ''
+		let input = a1
 
-		const template = await genHTML()
-		$('.printable').html(template)
-		window.print()
+		for (let i = 0; i < input.length; i++) {
+			if(Number.isInteger(parseInt(input[i]))){
+				count = await this.checkCount(input.slice(i, input.length))
+				if(input[i+(count.toString().length)] == '['){
+					i+=(count.toString().length)+1
+					let temp = ''
+					for(let j=i ; j< input.length ;j++,i++){				
+						if(await this.isAlpha(input[j])){
+							temp+=input[j]	
+						}
+						if(input[j] == ']'){
+							for(let k = 0 ; k<count ;k++){
+								print+=temp
+							}
+							break
+						}
+					}
+				}
+			
+			}
+			if(input[i] != ']'){
+				print+=input[i]
+			}
+		}
+		console.log("input=> ",input);
+		console.log("output => ",print);
+		
+	}
+
+	isAlpha = async (ch) => {
+		if ((ch >= "A" && ch <= "Z") || (ch >= "a" && ch <= "z")){
+			return true
+		}
+		return false
+	}
+
+	checkCount = async (num) => {
+		let tempNumber = ''
+		for(let i = 0 ; i< num.length;i++){
+			if(Number.isInteger(parseInt(num[i]))){
+				tempNumber+=num[i]
+			}
+			else
+				break
+		}
+		return parseInt(tempNumber)
+	}
+
+	checkThaiNumber = async (ch) => {
+		if (ch.charCodeAt(0) >= 3664) {
+			return true
+		}
+		return false
 	}
 
 	componentDidMount = async () => {
@@ -49,7 +106,7 @@ class App extends Component {
 						<div class="jumbotron">
 							<h1 class="display-3">Jumbotron heading</h1>
 							<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-							<p><a class="btn btn-lg btn-primary" href="#" style = {{width : "100%"}} onClick = {this.printPreview}>Print Invoice</a></p>
+							<p><a class="btn btn-lg btn-primary" href="#" style={{ width: "100%" }} onClick={this.printPreview}>Print Invoice</a></p>
 						</div>
 
 						<div class="row marketing">
@@ -76,7 +133,7 @@ class App extends Component {
 							</div>
 						</div>
 
-					
+
 
 					</div>
 
